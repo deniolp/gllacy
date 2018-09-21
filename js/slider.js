343c5ba8e4d0fcaec761217eb25d0d4318951436
Denis Popov <deniolp@gmail.com>
2018-09-21 12:38:56 +0300

    Реализация движения обоих range с помощью клавиатуры

--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,9 @@
 (function () {
   var RANGE_WIDTH = 176;
   var RANGE_LEFT_BORDER = 22;
+  var KEYCODE_LEFT = 37;
+  var KEYCODE_RIGHT = 39;
+  var RANGE_SCROLL_STEP = 5;
 
   var rangeLeft = document.querySelector('.runner-left');
   var rangeRight = document.querySelector('.runner-right');
@@ -45,6 +48,43 @@
     };
   };
 
+  var keyDownHandler = function (element, boolean, callback) {
+    return function(downEvt) {
+      var leftPosition;
+      if (downEvt.keyCode === KEYCODE_LEFT) {
+        leftPosition = element.offsetLeft - RANGE_SCROLL_STEP;
+        if (leftPosition < RANGE_LEFT_BORDER) {
+          element.style.left = RANGE_LEFT_BORDER + 'px';
+        } else {
+          element.style.left = leftPosition + 'px';
+        }
+        callback(boolean, leftPosition);
+      } else if (downEvt.keyCode === KEYCODE_RIGHT) {
+        leftPosition = element.offsetLeft + RANGE_SCROLL_STEP;
+        if (leftPosition > RANGE_WIDTH) {
+          element.style.left = RANGE_WIDTH + 'px';
+        } else {
+          element.style.left = leftPosition + 'px';
+        }
+        callback(boolean, leftPosition);
+      }
+    };
+  };
+
   rangeLeft.addEventListener('mousedown', mouseDownHandler(rangeLeft, true, window.ranges.applyRange));
   rangeRight.addEventListener('mousedown', mouseDownHandler(rangeRight, false, window.ranges.applyRange));
+
+  rangeLeft.addEventListener('focus', function () {
+    rangeLeft.addEventListener('keydown', keyDownHandler(rangeLeft, true, window.ranges.applyRange));
+  });
+  rangeRight.addEventListener('focus', function () {
+    rangeRight.addEventListener('keydown', keyDownHandler(rangeRight, false, window.ranges.applyRange));
+  });
+
+  rangeLeft.addEventListener('focusout', function () {
+    rangeLeft.removeEventListener('keydown', keyDownHandler);
+  });
+  rangeRight.addEventListener('focusout', function () {
+    rangeRight.removeEventListener('keydown', keyDownHandler);
+  });
 })();
